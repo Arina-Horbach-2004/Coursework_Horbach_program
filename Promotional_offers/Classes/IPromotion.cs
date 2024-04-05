dceeb25ed5af793a932986cbb63d9921a472de82
Arina Gorbach <a.a.horbach@student.khai.edu>
2024-04-05 14:17:42 +0200

    Створення інтерфейсу IPromotion

---
+++ Promotional_offers/Classes/IPromotion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promotional_offers.Classes
+{
+    public interface IPromotion
+    {
+        int ID { get; set; }
+        string Shop { get; set; }
+        string Category { get; set; }
+        string Code { get; set; }
+        DateTime ExpiryDate { get; set; }
+        string Photo { get; set; }
+        string Description { get; set; }
+
+        bool IsUsed { get; }
+
+        public bool UsePromotion(string promoCode);
+
+        public bool EditPromotion(int id, string shop, string category, string code, DateTime expiryDate, string photo, string description);
+
+        public void DisplayDetails(bool isAuthenticated);
+    }
+}
