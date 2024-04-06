00b30de2622205d7ce722d1eb6be3df818c80a3b
Arina Gorbach <a.a.horbach@student.khai.edu>
2024-04-06 21:36:50 +0200

    Додавання інтерфейсу IPromotionSearch для пошуку акцій за категорією та ключовими словами

---
+++ Promotional_offers/Classes/IPromotionSearch.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promotional_offers.Classes
+{
+    public interface IPromotionSearch
+    {
+        List<Promotion> SearchPromotions(string category, string keywords, List<Promotion> promotions);
+    }
+}
