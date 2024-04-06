797ff65a29353ec19dd447660982eec7f5bc2811
Arina Gorbach <a.a.horbach@student.khai.edu>
2024-04-06 21:47:13 +0200

    Створення класу для роботи з файлами JSON

---
+++ Promotional_offers/Classes/JsonFileManager.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promotional_offers.Classes
+{
+    public class JsonFileManager
+    {
+        // Зберегти об'єкт у файл JSON
+        public static void SaveToJson<T>(List<T> list, string filePath)
+        {
+            string jsonData = JsonConvert.SerializeObject(list, Newtonsoft.Json.Formatting.Indented);
+            File.WriteAllText(filePath, jsonData);
+        }
+
+        // Зчитати об'єкт з файлу JSON
+        public static List<T> LoadFromJson<T>(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                string jsonData = File.ReadAllText(filePath);
+                return JsonConvert.DeserializeObject<List<T>>(jsonData);
+            }
+            else
+            {
+                return new List<T>();
+            }
+        }
+    }
+}
