53463c1b8b252630f014937c11c5fb6a3f1e9992
Arina Gorbach <a.a.horbach@student.khai.edu>
2024-04-06 21:34:04 +0200

    Додавання інтерфейсу IAdmin з методами управління акціями та користувачами

---
+++ Promotional_offers/Classes/IAdmin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promotional_offers.Classes
+{
+    public interface IAdmin
+    {
+        string Email { get; set; }
+        string Password { get; set; }
+
+        bool Authenticate(string login, string password);
+
+        bool CreatePromotion(int id, string store, string category, string promoCode, DateTime expirationDate, string photo, string description);
+
+        bool EditPromotion(int id, string store, string category, string promoCode, DateTime expirationDate, string photo, string description);
+
+        bool DeletePromotion(int id);
+
+        List<Promotion> GetValidPromotions();
+
+        List<Promotion> GetDeletedPromotions();
+
+        List<RegisteredUser> GetRegisteredUsers();
+    }
+}
