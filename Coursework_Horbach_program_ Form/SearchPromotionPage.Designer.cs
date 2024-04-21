d0685b5788262a0b8952469181b61e70dfa755bb
Arina Gorbach <a.a.horbach@student.khai.edu>
2024-04-22 01:51:58 +0200

    Реалізація інтерфейсу користувача "Пошук".

--- Coursework_Horbach_program_ Form/SearchPromotionPage.Designer.cs
+++ Coursework_Horbach_program_ Form/SearchPromotionPage.Designer.cs
@@ -28,12 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "SearchPromotionPage";
+            menuStrip1 = new MenuStrip();
+            homepageToolStripMenuItem = new ToolStripMenuItem();
+            acctionToolStripMenuItem = new ToolStripMenuItem();
+            searchToolStripMenuItem = new ToolStripMenuItem();
+            button_log_exit = new Button();
+            textBox_word = new TextBox();
+            comboBox_category = new ComboBox();
+            button_search = new Button();
+            panel_acction = new Panel();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { homepageToolStripMenuItem, acctionToolStripMenuItem, searchToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 28);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // homepageToolStripMenuItem
+            // 
+            homepageToolStripMenuItem.Font = new Font("Times New Roman", 10.8F);
+            homepageToolStripMenuItem.Name = "homepageToolStripMenuItem";
+            homepageToolStripMenuItem.Size = new Size(85, 24);
+            homepageToolStripMenuItem.Text = "Головна";
+            homepageToolStripMenuItem.Click += homepageToolStripMenuItem_Click;
+            // 
+            // acctionToolStripMenuItem
+            // 
+            acctionToolStripMenuItem.Font = new Font("Times New Roman", 10.8F);
+            acctionToolStripMenuItem.Name = "acctionToolStripMenuItem";
+            acctionToolStripMenuItem.Size = new Size(62, 24);
+            acctionToolStripMenuItem.Text = "Акції";
+            acctionToolStripMenuItem.Click += acctionToolStripMenuItem_Click;
+            // 
+            // searchToolStripMenuItem
+            // 
+            searchToolStripMenuItem.Font = new Font("Times New Roman", 10.8F);
+            searchToolStripMenuItem.Name = "searchToolStripMenuItem";
+            searchToolStripMenuItem.Size = new Size(77, 24);
+            searchToolStripMenuItem.Text = "Пошук";
+            searchToolStripMenuItem.Click += searchToolStripMenuItem_Click;
+            // 
+            // button_log_exit
+            // 
+            button_log_exit.Font = new Font("Times New Roman", 10.8F);
+            button_log_exit.Location = new Point(578, 0);
+            button_log_exit.Name = "button_log_exit";
+            button_log_exit.Size = new Size(197, 28);
+            button_log_exit.TabIndex = 15;
+            button_log_exit.Text = "Реєстрація/авторизація";
+            button_log_exit.UseVisualStyleBackColor = true;
+            button_log_exit.Click += button_log_exit_Click;
+            // 
+            // textBox_word
+            // 
+            textBox_word.Font = new Font("Times New Roman", 13.8F);
+            textBox_word.Location = new Point(121, 70);
+            textBox_word.Name = "textBox_word";
+            textBox_word.Size = new Size(385, 34);
+            textBox_word.TabIndex = 16;
+            // 
+            // comboBox_category
+            // 
+            comboBox_category.Font = new Font("Times New Roman", 13.8F);
+            comboBox_category.FormattingEnabled = true;
+            comboBox_category.Location = new Point(121, 123);
+            comboBox_category.Name = "comboBox_category";
+            comboBox_category.Size = new Size(385, 34);
+            comboBox_category.TabIndex = 17;
+            // 
+            // button_search
+            // 
+            button_search.Font = new Font("Times New Roman", 13.8F);
+            button_search.Location = new Point(600, 67);
+            button_search.Name = "button_search";
+            button_search.Size = new Size(130, 37);
+            button_search.TabIndex = 18;
+            button_search.Text = "Пошук";
+            button_search.UseVisualStyleBackColor = true;
+            button_search.Click += button_search_Click;
+            // 
+            // panel_acction
+            // 
+            panel_acction.Location = new Point(0, 168);
+            panel_acction.Name = "panel_acction";
+            panel_acction.Size = new Size(800, 282);
+            panel_acction.TabIndex = 19;
+            // 
+            // SearchPromotionPage
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel_acction);
+            Controls.Add(button_search);
+            Controls.Add(comboBox_category);
+            Controls.Add(textBox_word);
+            Controls.Add(button_log_exit);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "SearchPromotionPage";
+            Text = "SearchPromotionPage";
+            Load += SearchPromotionPage_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem homepageToolStripMenuItem;
+        private ToolStripMenuItem acctionToolStripMenuItem;
+        private ToolStripMenuItem searchToolStripMenuItem;
+        private Button button_log_exit;
+        private TextBox textBox_word;
+        private ComboBox comboBox_category;
+        private Button button_search;
+        private Panel panel_acction;
     }
 }
--- Coursework_Horbach_program_ Form/SearchPromotionPage.cs
+++ Coursework_Horbach_program_ Form/SearchPromotionPage.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Promotional_offers.Classes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System;
+using System.Linq;
 
 namespace Coursework_Horbach_program__Form
 {
@@ -23,6 +26,180 @@ namespace Coursework_Horbach_program__Form
             this.login = login;
             this.password = password;
             this.isAuthenticated = isAuthenticated;
+        }
+
+        private void button_search_Click(object sender, EventArgs e)
+        {
+            string category = comboBox_category.Text;
+            string keywords = textBox_word.Text;
+
+            Admin admin = new Admin("", "");
+            var listPromotions = admin.GetValidPromotions();
+            panel_acction.Controls.Clear();
+            if (isAuthenticated)
+            {
+                RegisteredUser registeredUser = RegisteredUser.registeredUsers.FirstOrDefault(u => u.Email == login && u.Password == password);
+                var filteredPromotions1 = registeredUser.SearchPromotions(category, keywords, listPromotions);
+
+                panel_acction.AutoScroll = true;
+                int pictureBoxY = 14;
+                int labelDescriptionY = 35;
+                int buttonGoToActionY = 20;
+
+                foreach (var promotion in filteredPromotions1)
+                {
+                    PictureBox pictureBox = new PictureBox();
+                    Label labelDescription = new Label();
+                    Button buttonGoToAction = new Button();
+
+                    pictureBox.ImageLocation = promotion.Photo;
+                    pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+                    pictureBox.Location = new Point(15, pictureBoxY);
+                    pictureBox.Width = 125;
+                    pictureBox.Height = 62;
+                    panel_acction.Controls.Add(pictureBox);
+
+                    labelDescription.Text = promotion.Description;
+                    labelDescription.Location = new Point(176, labelDescriptionY);
+                    labelDescription.Width = 67;
+                    labelDescription.Height = 26;
+                    panel_acction.Controls.Add(labelDescription);
+
+                    buttonGoToAction.Text = "До акції";
+                    buttonGoToAction.Tag = promotion;
+                    buttonGoToAction.Location = new Point(656, buttonGoToActionY);
+                    buttonGoToAction.Width = 101;
+                    buttonGoToAction.Height = 41;
+                    panel_acction.Controls.Add(buttonGoToAction);
+
+                    buttonGoToAction.Click += buttonGoToAction_Click;
+
+                    pictureBoxY += 100;
+                    labelDescriptionY += 95;
+                    buttonGoToActionY += 94;
+                    ClearPromotionFields();
+                }
+
+            }
+            else
+            {
+                Guest guest = new Guest();
+                var filteredPromotions2 = guest.SearchPromotions(category, keywords, listPromotions);
+
+                panel_acction.AutoScroll = true;
+                int pictureBoxY = 14;
+                int labelDescriptionY = 35;
+                int buttonGoToActionY = 20;
+
+                foreach (var promotion in filteredPromotions2)
+                {
+                    PictureBox pictureBox = new PictureBox();
+                    Label labelDescription = new Label();
+                    Button buttonGoToAction = new Button();
+
+                    pictureBox.ImageLocation = promotion.Photo;
+                    pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+                    pictureBox.Location = new Point(15, pictureBoxY);
+                    pictureBox.Width = 125;
+                    pictureBox.Height = 62;
+                    panel_acction.Controls.Add(pictureBox);
+
+                    labelDescription.Text = promotion.Description;
+                    labelDescription.Location = new Point(176, labelDescriptionY);
+                    labelDescription.Width = 67;
+                    labelDescription.Height = 26;
+                    panel_acction.Controls.Add(labelDescription);
+
+                    buttonGoToAction.Text = "До акції";
+                    buttonGoToAction.Tag = promotion;
+                    buttonGoToAction.Location = new Point(656, buttonGoToActionY);
+                    buttonGoToAction.Width = 101;
+                    buttonGoToAction.Height = 41;
+                    panel_acction.Controls.Add(buttonGoToAction);
+                    buttonGoToAction.Click += buttonGoToAction_Click;
+
+                    pictureBoxY += 100;
+                    labelDescriptionY += 95;
+                    buttonGoToActionY += 94;
+                    ClearPromotionFields();
+                }
+            }
+
+        }
+
+        private void ClearPromotionFields()
+        {
+
+            comboBox_category.SelectedIndex = -1;
+            textBox_word.Text = "";
+        }
+
+        private void buttonGoToAction_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            Promotion promotion = (Promotion)button.Tag;
+
+            ViewdetailsPromotionPage viewPromotion = new ViewdetailsPromotionPage(promotion.ID, promotion.Shop, promotion.Category, promotion.Description, promotion.Photo, promotion.ExpiryDate, promotion.Code, login, password, isAuthenticated);
+            viewPromotion.Show();
+            this.Hide();
+        }
+
+        private void SearchPromotionPage_Load(object sender, EventArgs e)
+        {
+            if (isAuthenticated)
+            {
+                button_log_exit.Text = "Вийти";
+            }
+            else
+            {
+                button_log_exit.Text = "Реєстрація/авторизація";
+            }
+            List<string> categories = new List<string>();
+            categories.Add("Прикраси");
+            categories.Add("Ігри");
+            categories.Add("Їжа");
+            categories.Add("Одяг та взуття");
+            categories.Add("Зоотовари");
+            categories.Add("Книги");
+            categories.Add("Фільми та серіали");
+            categories.Add("Музика");
+
+            comboBox_category.Items.Clear();
+
+            foreach (string category in categories)
+            {
+                comboBox_category.Items.Add(category);
+            }
+        }
+
+        private void homepageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (isAuthenticated)
+            {
+                HomePage home = new HomePage(login, password, isAuthenticated);
+                home.Show();
+                this.Hide();
+            }
+            else
+            {
+                HomePage home = new HomePage(login, password, isAuthenticated);
+                home.Show();
+                this.Hide();
+            }
+        }
+
+        private void acctionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AcctionPage acctionPage = new AcctionPage(login, password, isAuthenticated);
+            acctionPage.Show();
+            this.Hide();
+        }
+
+        private void button_log_exit_Click(object sender, EventArgs e)
+        {
+            Register_or_AuthenticatePage loginForm = new Register_or_AuthenticatePage(false);
+            loginForm.Show();
+            this.Hide();
         }
     }
 }
