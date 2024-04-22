57d2d07b8cdcb08abcbee50c294487ccdc31b4a1
Arina Gorbach <a.a.horbach@student.khai.edu>
2024-04-22 02:37:21 +0200

    Реалізація інтерфейсу користувача сторінка адміністратора.

--- Coursework_Horbach_program_ Form/AdminPage.Designer.cs
+++ Coursework_Horbach_program_ Form/AdminPage.Designer.cs
@@ -28,12 +28,155 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "AdminPage";
+            menuStrip1 = new MenuStrip();
+            addpromotionToolStripMenuItem = new ToolStripMenuItem();
+            editpromotionToolStripMenuItem = new ToolStripMenuItem();
+            deletepromotionToolStripMenuItem = new ToolStripMenuItem();
+            файлToolStripMenuItem = new ToolStripMenuItem();
+            активніПропозиціїToolStripMenuItem = new ToolStripMenuItem();
+            зберегтиToolStripMenuItem = new ToolStripMenuItem();
+            завантажитиToolStripMenuItem = new ToolStripMenuItem();
+            видаленіПромокодиToolStripMenuItem = new ToolStripMenuItem();
+            зберегтиToolStripMenuItem1 = new ToolStripMenuItem();
+            завантажитиToolStripMenuItem1 = new ToolStripMenuItem();
+            givelistToolStripMenuItem = new ToolStripMenuItem();
+            button_log_exit = new Button();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { addpromotionToolStripMenuItem, editpromotionToolStripMenuItem, deletepromotionToolStripMenuItem, файлToolStripMenuItem, givelistToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 28);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // addpromotionToolStripMenuItem
+            // 
+            addpromotionToolStripMenuItem.Font = new Font("Times New Roman", 7.8F);
+            addpromotionToolStripMenuItem.Name = "addpromotionToolStripMenuItem";
+            addpromotionToolStripMenuItem.Size = new Size(131, 24);
+            addpromotionToolStripMenuItem.Text = "Додати пророзицію";
+            addpromotionToolStripMenuItem.Click += addpromotionToolStripMenuItem_Click;
+            // 
+            // editpromotionToolStripMenuItem
+            // 
+            editpromotionToolStripMenuItem.Font = new Font("Times New Roman", 7.8F);
+            editpromotionToolStripMenuItem.Name = "editpromotionToolStripMenuItem";
+            editpromotionToolStripMenuItem.Size = new Size(152, 24);
+            editpromotionToolStripMenuItem.Text = "Редагувати пропозицію";
+            editpromotionToolStripMenuItem.Click += editpromotionToolStripMenuItem_Click;
+            // 
+            // deletepromotionToolStripMenuItem
+            // 
+            deletepromotionToolStripMenuItem.Font = new Font("Times New Roman", 7.8F);
+            deletepromotionToolStripMenuItem.Name = "deletepromotionToolStripMenuItem";
+            deletepromotionToolStripMenuItem.Size = new Size(143, 24);
+            deletepromotionToolStripMenuItem.Text = "Видалити пропозицію";
+            deletepromotionToolStripMenuItem.Click += deletepromotionToolStripMenuItem_Click;
+            // 
+            // файлToolStripMenuItem
+            // 
+            файлToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { активніПропозиціїToolStripMenuItem, видаленіПромокодиToolStripMenuItem });
+            файлToolStripMenuItem.Font = new Font("Times New Roman", 7.8F);
+            файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            файлToolStripMenuItem.Size = new Size(50, 24);
+            файлToolStripMenuItem.Text = "Файл";
+            // 
+            // активніПропозиціїToolStripMenuItem
+            // 
+            активніПропозиціїToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { зберегтиToolStripMenuItem, завантажитиToolStripMenuItem });
+            активніПропозиціїToolStripMenuItem.Name = "активніПропозиціїToolStripMenuItem";
+            активніПропозиціїToolStripMenuItem.Size = new Size(224, 26);
+            активніПропозиціїToolStripMenuItem.Text = "Активні промокоди";
+            // 
+            // зберегтиToolStripMenuItem
+            // 
+            зберегтиToolStripMenuItem.Name = "зберегтиToolStripMenuItem";
+            зберегтиToolStripMenuItem.Size = new Size(159, 26);
+            зберегтиToolStripMenuItem.Text = "Зберегти";
+            зберегтиToolStripMenuItem.Click += зберегтиToolStripMenuItem_Click;
+            // 
+            // завантажитиToolStripMenuItem
+            // 
+            завантажитиToolStripMenuItem.Name = "завантажитиToolStripMenuItem";
+            завантажитиToolStripMenuItem.Size = new Size(159, 26);
+            завантажитиToolStripMenuItem.Text = "Завантажити";
+            завантажитиToolStripMenuItem.Click += завантажитиToolStripMenuItem_Click;
+            // 
+            // видаленіПромокодиToolStripMenuItem
+            // 
+            видаленіПромокодиToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { зберегтиToolStripMenuItem1, завантажитиToolStripMenuItem1 });
+            видаленіПромокодиToolStripMenuItem.Name = "видаленіПромокодиToolStripMenuItem";
+            видаленіПромокодиToolStripMenuItem.Size = new Size(224, 26);
+            видаленіПромокодиToolStripMenuItem.Text = "Видалені промокоди";
+            // 
+            // зберегтиToolStripMenuItem1
+            // 
+            зберегтиToolStripMenuItem1.Name = "зберегтиToolStripMenuItem1";
+            зберегтиToolStripMenuItem1.Size = new Size(224, 26);
+            зберегтиToolStripMenuItem1.Text = "Зберегти";
+            зберегтиToolStripMenuItem1.Click += зберегтиToolStripMenuItem1_Click;
+            // 
+            // завантажитиToolStripMenuItem1
+            // 
+            завантажитиToolStripMenuItem1.Name = "завантажитиToolStripMenuItem1";
+            завантажитиToolStripMenuItem1.Size = new Size(224, 26);
+            завантажитиToolStripMenuItem1.Text = "Завантажити";
+            завантажитиToolStripMenuItem1.Click += завантажитиToolStripMenuItem1_Click;
+            // 
+            // givelistToolStripMenuItem
+            // 
+            givelistToolStripMenuItem.Font = new Font("Times New Roman", 7.8F);
+            givelistToolStripMenuItem.Name = "givelistToolStripMenuItem";
+            givelistToolStripMenuItem.Size = new Size(118, 24);
+            givelistToolStripMenuItem.Text = "Отримати списки";
+            givelistToolStripMenuItem.Click += givelistToolStripMenuItem_Click;
+            // 
+            // button_log_exit
+            // 
+            button_log_exit.Font = new Font("Times New Roman", 10.8F);
+            button_log_exit.Location = new Point(635, 0);
+            button_log_exit.Name = "button_log_exit";
+            button_log_exit.Size = new Size(152, 28);
+            button_log_exit.TabIndex = 15;
+            button_log_exit.Text = "Вийти";
+            button_log_exit.UseVisualStyleBackColor = true;
+            button_log_exit.Click += button_log_exit_Click;
+            // 
+            // AdminPage
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button_log_exit);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "AdminPage";
+            Text = "AdminPage";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem addpromotionToolStripMenuItem;
+        private ToolStripMenuItem editpromotionToolStripMenuItem;
+        private ToolStripMenuItem deletepromotionToolStripMenuItem;
+        private ToolStripMenuItem файлToolStripMenuItem;
+        private ToolStripMenuItem активніПропозиціїToolStripMenuItem;
+        private ToolStripMenuItem зберегтиToolStripMenuItem;
+        private ToolStripMenuItem завантажитиToolStripMenuItem;
+        private ToolStripMenuItem видаленіПромокодиToolStripMenuItem;
+        private ToolStripMenuItem зберегтиToolStripMenuItem1;
+        private ToolStripMenuItem завантажитиToolStripMenuItem1;
+        private ToolStripMenuItem givelistToolStripMenuItem;
+        private Button button_log_exit;
     }
 }
--- Coursework_Horbach_program_ Form/AdminPage.cs
+++ Coursework_Horbach_program_ Form/AdminPage.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Promotional_offers.Classes;
 using System.Windows.Forms;
 
 namespace Coursework_Horbach_program__Form
@@ -20,6 +21,97 @@ namespace Coursework_Horbach_program__Form
         {
             InitializeComponent();
             this.isAuthenticated = isAuthenticated;
+        }
+
+        private void addpromotionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AddPromotionPage add = new AddPromotionPage();
+            add.Show();
+            this.Hide();
+        }
+
+        private void editpromotionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            EditPromotionPage edit = new EditPromotionPage();
+            edit.Show();
+            this.Hide();
+        }
+
+        private void deletepromotionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DeletePromotionPage deletePromotionPage = new DeletePromotionPage();
+            deletePromotionPage.Show();
+            this.Hide();
+        }
+
+        private void button_log_exit_Click(object sender, EventArgs e)
+        {
+            Register_or_AuthenticatePage register = new Register_or_AuthenticatePage(false);
+            register.Show();
+            this.Hide();
+        }
+
+        private void зберегтиToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "JSON files (*.json)|*.json";
+            saveFileDialog.Title = "Save JSON File";
+            saveFileDialog.ShowDialog();
+            if (saveFileDialog.FileName != "")
+            {
+                string filepath = saveFileDialog.FileName;
+                Admin admin = new Admin("", "");
+                admin.SavePromotionsToJson(filepath);
+            }
+        }
+
+        private void завантажитиToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "JSON files (*.json)|*.json";
+            openFileDialog.ShowDialog();
+            if (openFileDialog.FileName != "")
+            {
+                string filepath = openFileDialog.FileName;
+                Admin admin = new Admin("", "");
+                admin.LoadPromotionsFromJson(filepath);
+            }
+        }
+
+        private void зберегтиToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "JSON files (*.json)|*.json";
+            saveFileDialog.Title = "Save JSON File";
+            saveFileDialog.ShowDialog();
+            if (saveFileDialog.FileName != "")
+            {
+                string filepath = saveFileDialog.FileName;
+                Admin admin = new Admin("", "");
+                admin.SaveDeletedPromotionsToJson(filepath);
+            }
+
+        }
+
+        private void завантажитиToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "JSON files (*.json)|*.json";
+            openFileDialog.Title = "Open JSON File";
+            openFileDialog.ShowDialog();
+            if (openFileDialog.FileName != "")
+            {
+                string filepath = openFileDialog.FileName;
+                Admin admin = new Admin("", "");
+                admin.LoadDeletedPromotionsFromJson(filepath);
+            }
+        }
+
+        private void givelistToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FileManagePage fileManagePage = new FileManagePage();
+            fileManagePage.Show();
+            this.Hide();
         }
     }
 }
