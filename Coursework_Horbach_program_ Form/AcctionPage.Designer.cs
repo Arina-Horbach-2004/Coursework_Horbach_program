542ff400a6fbcc86b9dbe1256c325630a253ea0a
Arina Gorbach <a.a.horbach@student.khai.edu>
2024-04-22 02:02:12 +0200

    Реалізація інтерфейсу користувача "Акції".

--- Coursework_Horbach_program_ Form/AcctionPage.Designer.cs
+++ Coursework_Horbach_program_ Form/AcctionPage.Designer.cs
@@ -28,12 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "AcctionPage";
+            menuStrip1 = new MenuStrip();
+            homepageToolStripMenuItem = new ToolStripMenuItem();
+            acctionToolStripMenuItem = new ToolStripMenuItem();
+            searchToolStripMenuItem = new ToolStripMenuItem();
+            button_log_exit = new Button();
+            panel_acction = new Panel();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Font = new Font("Times New Roman", 10.8F);
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { homepageToolStripMenuItem, acctionToolStripMenuItem, searchToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 28);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // homepageToolStripMenuItem
+            // 
+            homepageToolStripMenuItem.Name = "homepageToolStripMenuItem";
+            homepageToolStripMenuItem.Size = new Size(85, 24);
+            homepageToolStripMenuItem.Text = "Головна";
+            homepageToolStripMenuItem.Click += homepageToolStripMenuItem_Click;
+            // 
+            // acctionToolStripMenuItem
+            // 
+            acctionToolStripMenuItem.Name = "acctionToolStripMenuItem";
+            acctionToolStripMenuItem.Size = new Size(62, 24);
+            acctionToolStripMenuItem.Text = "Акції";
+            // 
+            // searchToolStripMenuItem
+            // 
+            searchToolStripMenuItem.Name = "searchToolStripMenuItem";
+            searchToolStripMenuItem.Size = new Size(77, 24);
+            searchToolStripMenuItem.Text = "Пошук";
+            searchToolStripMenuItem.Click += searchToolStripMenuItem_Click;
+            // 
+            // button_log_exit
+            // 
+            button_log_exit.Font = new Font("Times New Roman", 10.8F);
+            button_log_exit.Location = new Point(578, 0);
+            button_log_exit.Name = "button_log_exit";
+            button_log_exit.Size = new Size(197, 28);
+            button_log_exit.TabIndex = 15;
+            button_log_exit.Text = "Реєстрація/авторизація";
+            button_log_exit.UseVisualStyleBackColor = true;
+            button_log_exit.Click += button_log_exit_Click;
+            // 
+            // panel_acction
+            // 
+            panel_acction.Location = new Point(0, 28);
+            panel_acction.Name = "panel_acction";
+            panel_acction.Size = new Size(800, 422);
+            panel_acction.TabIndex = 16;
+            // 
+            // AcctionPage
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel_acction);
+            Controls.Add(button_log_exit);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "AcctionPage";
+            Text = "AcctionPage";
+            Load += AcctionPage_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem homepageToolStripMenuItem;
+        private ToolStripMenuItem acctionToolStripMenuItem;
+        private ToolStripMenuItem searchToolStripMenuItem;
+        private Button button_log_exit;
+        private Panel panel_acction;
     }
 }
--- Coursework_Horbach_program_ Form/AcctionPage.cs
+++ Coursework_Horbach_program_ Form/AcctionPage.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Promotional_offers.Classes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -23,6 +24,96 @@ namespace Coursework_Horbach_program__Form
             this.login = login;
             this.password = password;
             this.isAuthenticated = isAuthenticated;
+        }
+
+        private void AcctionPage_Load(object sender, EventArgs e)
+        {
+            if (isAuthenticated)
+            {
+                button_log_exit.Text = "Вийти";
+            }
+            else
+            {
+                button_log_exit.Text = "Реєстрація/авторизація";
+            }
+            Admin admin = new Admin("", "");
+            var listPromotions = admin.GetValidPromotions();
+            int pictureBoxY = 13;
+            int labelDescriptionY = 28;
+            int buttonGoToActionY = 13;
+
+            foreach (var promotion in listPromotions)
+            {
+                PictureBox pictureBox = new PictureBox();
+                Label labelDescription = new Label();
+                Button buttonGoToAction = new Button();
+
+                pictureBox.ImageLocation = promotion.Photo;
+                pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+                pictureBox.Location = new Point(12, pictureBoxY);
+                pictureBox.Width = 125;
+                pictureBox.Height = 62;
+                panel_acction.Controls.Add(pictureBox);
+
+                labelDescription.Text = promotion.Description;
+                labelDescription.Location = new Point(203, labelDescriptionY);
+                labelDescription.Width = 67;
+                labelDescription.Height = 26;
+                panel_acction.Controls.Add(labelDescription);
+
+                buttonGoToAction.Text = "До акції";
+                buttonGoToAction.Tag = promotion;
+                buttonGoToAction.Location = new Point(666, buttonGoToActionY);
+                buttonGoToAction.Width = 101;
+                buttonGoToAction.Height = 41;
+                panel_acction.Controls.Add(buttonGoToAction);
+
+                buttonGoToAction.Click += buttonGoToAction_Click;
+
+                pictureBoxY += 79;
+                labelDescriptionY += 79;
+                buttonGoToActionY += 79;
+
+            }
+        }
+
+        private void buttonGoToAction_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            Promotion promotion = (Promotion)button.Tag;
+            ViewdetailsPromotionPage viewPromotion = new ViewdetailsPromotionPage(promotion.ID, promotion.Shop, promotion.Category, promotion.Description, promotion.Photo, promotion.ExpiryDate, promotion.Code, login, password, isAuthenticated);
+            viewPromotion.Show();
+            this.Hide();
+        }
+
+        private void button_log_exit_Click(object sender, EventArgs e)
+        {
+            Register_or_AuthenticatePage loginForm = new Register_or_AuthenticatePage(false);
+            loginForm.Show();
+            this.Hide();
+        }
+
+        private void homepageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (isAuthenticated)
+            {
+                HomePage home = new HomePage(login, password, isAuthenticated);
+                home.Show();
+                this.Hide();
+            }
+            else
+            {
+                HomePage home = new HomePage(login, password, isAuthenticated);
+                home.Show();
+                this.Hide();
+            }
+        }
+
+        private void searchToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SearchPromotionPage searchpage = new SearchPromotionPage(login, password, false);
+            searchpage.Show();
+            this.Hide();
         }
     }
 }
